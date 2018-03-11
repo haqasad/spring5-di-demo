42a09baf7a94c009aee7d5e5f8602355cb11f6ea
haqasad <haq.asad000@gmail.com>
2018-03-10 22:31:27 -0500

    Constructor injection documentation

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -6,6 +6,17 @@ import guru.springframework.services.GreetingServiceImpl;
 /**
  * Created by jt on 5/24/17.
  */
+
+//================================================================================
+//                  DEPENDENCY INJECTION BY CONSTRUCTOR
+//================================================================================
+// This is the most preferred way of DI
+//
+// Create an interface and declare the interface as a private property in
+// dependency injection class
+//
+// Create a constructor using the private property
+//================================================================================
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
