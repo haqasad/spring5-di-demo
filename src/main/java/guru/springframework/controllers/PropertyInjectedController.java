c6ea5f88cd4cd742e229506e42b1baff1439e9c7
haqasad <haq.asad000@gmail.com>
2018-03-10 22:25:51 -0500

    Property injection documentation

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -7,8 +7,17 @@ import guru.springframework.services.GreetingServiceImpl;
  * Created by jt on 5/24/17.
  */
 
+//================================================================================
+//                  DEPENDENCY INJECTION BY CLASS PROPERTY
+//================================================================================
+// This is the least preferred way of DI. This is an example of how not to do DI
+//================================================================================
 public class PropertyInjectedController {
 
+    //============================================================================
+    // NOTICE: A public property of type GreetingServiceImpl is declared but no
+    // object is created using 'new' keyword in this class
+    //============================================================================
     public GreetingServiceImpl greetingService;
 
     public String sayHello(){
