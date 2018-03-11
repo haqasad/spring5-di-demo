98e29236cacb761d160e4d512efe9c09249e48ed
haqasad <haq.asad000@gmail.com>
2018-03-10 22:34:54 -0500

    GreetingServiceImpl documentation

--- src/main/java/guru/springframework/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/services/GreetingServiceImpl.java
@@ -5,7 +5,16 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 5/24/17.
  */
+
+//==========================================================
+// @Service annotation means that this is a Spring component
+//==========================================================
 @Service
+//==========================================================================
+// GreetingServiceImpl is an implementation of the interface GreetingService
+//
+// Check out the interface GreetingService
+//==========================================================================
 public class GreetingServiceImpl implements GreetingService {
 
     public static final String HELLO_GURUS = "Hello Gurus!!!!";
