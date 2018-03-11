1e380d9299ddd942134a5268438580d390ecfd2c
haqasad <haq.asad000@gmail.com>
2018-03-10 22:29:05 -0500

    Setter injection documentation

--- src/main/java/guru/springframework/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -6,7 +6,25 @@ import guru.springframework.services.GreetingServiceImpl;
 /**
  * Created by jt on 5/24/17.
  */
+
+//================================================================================
+//                  DEPENDENCY INJECTION BY SETTERS
+//================================================================================
+// This is the least preferred way of DI. This is an example of how not to do DI
+//================================================================================
 public class SetterInjectedController {
+
+    //============================================================================
+    // A private property of type GreetingService is declared
+    //
+    // GreetingService is an Interface and is used directly as a property
+    //
+    // To access this property a setter is used
+    //
+    // For property injected dependency a public property was declared and the
+    // property was an implemented class of an interface but for setter injected
+    // dependency a private property is declared and the property is the interface
+    //============================================================================
     private GreetingService greetingService;
 
     public String sayHello(){
