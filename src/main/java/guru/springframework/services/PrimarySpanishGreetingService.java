0e31dd40a00a4b2765da33ec0fa23d3151ce7666
haqasad <haq.asad000@gmail.com>
2018-03-11 14:31:29 -0400

    Using Profiles

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -9,6 +9,20 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @Profile("es")
+//================================================================================
+// @Profile allows to choose among different profiles and run the chosen profile(s)
+// at run time
+//================================================================================
+// In this example, other than four different implementations of GreetingService a
+// new implementation has been added which prints the greeting in Spanish
+//================================================================================
+// @Profile takes a string that makes the bean unique from other beans
+// For English @Profile takes "en' and for Spanish it takes "es"
+//================================================================================
+// Both the English and Spanish greeting beans are marked @Primary as well
+// The one that is needed to be run at run time must be specified at application.
+// properties
+//================================================================================
 @Primary
 public class PrimarySpanishGreetingService implements GreetingService {
 
